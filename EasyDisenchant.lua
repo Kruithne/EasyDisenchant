0423ffdba57b3b41fda7d845783cd49dd25e95f8
Kruithne <kruithne@gmail.com>
2016-11-06 00:01:21 +0000

    Finish the add-on.

--- EasyDisenchant.lua
+++ EasyDisenchant.lua
@@ -6,6 +6,7 @@ local _K = Krutilities;
 local _M = {
 	addonName = "EasyDisenchant",
 	eventFrame = CreateFrame("FRAME"),
+	itemButtons = {},
 	eventMap = {}, -- Used for internal mapping of events.
 	isTradeSkillFrameHooked = false,
 	tradeSkillID = 333, -- CHARACTER_PROFESSION_ENCHANTING
@@ -77,6 +78,49 @@ _M.CheckTradeSkillButton = function(self)
 	end
 end
 
+_M.UpdateItems = function(self)
+	-- Hide buttons.
+	local buttons = _M.itemButtons;
+	local nButtons = #buttons;
+
+	for i = 1, nButtons do
+		buttons[i]:Hide();
+	end
+
+	local disenchantName = GetSpellInfo(13262);
+	local macroFormat = "/stopcasting\n/cast %s\n/cast %s %s";
+
+	local useButton = 1;
+	for bagID = 0, NUM_BAG_SLOTS do
+		for slotID = 1, GetContainerNumSlots(bagID) do
+			local itemTexture, _, _, itemQuality, _, _, itemLink = GetContainerItemInfo(bagID, slotID);
+
+			-- Skip non-existant items or legendary+.
+			if itemLink ~= nil and (itemQuality ~= nil and itemQuality < 5) then
+				local itemName, _, _, _, _, itemClass = GetItemInfo(itemLink);
+
+				-- Only disenchant weapons and armour.
+				if itemClass == WEAPON or itemClass == ARMOR then
+					local button = buttons[useButton];
+
+					SetItemButtonTexture(button, itemTexture);
+					SetItemButtonQuality(button, itemQuality, itemLink);
+
+					button:SetAttribute("macrotext", macroFormat:format(disenchantName, bagID, slotID));
+					button.link = itemLink;
+					button:Show();
+
+					if useButton == nButtons then
+						return;
+					end
+
+					useButton = useButton + 1;
+				end
+			end
+		end
+	end
+end
+
 _M.CreateDisenchantFrame = function(self)
 	local bgAnchor = {
 		{ point = "TOPLEFT", x = 8, y = -8 },
@@ -108,14 +152,12 @@ _M.CreateDisenchantFrame = function(self)
 
 	self.disenchantFrame = _K:Frame({
 		name = "EasyDisenchantFrame",
-		size = {418, 236},
-		frames = {
-			{ -- Close Button
-				type = "BUTTON",
-				parentName = "CloseButton",
-				inherit = "UIPanelCloseButton",
-				points = { point = "TOPRIGHT", x = -20, y = -25 }
-			}
+		size = {418, 472},
+		frames = { -- Close Button
+			type = "BUTTON",
+			parentName = "CloseButton",
+			inherit = "UIPanelCloseButton",
+			points = { point = "TOPRIGHT", x = -20, y = -25 }
 		},
 		textures = {
 			{ -- Background.
@@ -186,8 +228,89 @@ _M.CreateDisenchantFrame = function(self)
 				},
 				texCoord = {0.015625, 0.375, 0, 1}
 			}
+		},
+		scripts = {
+			OnHide = function() PlaySound("UI_EtherealWindow_Close"); end
 		}
 	});
+
+	local glow = _K:Frame({
+		size = 37,
+		hidden = true,
+		parent = self.disenchantFrame,
+		textures = {
+			{
+				parentName = "InnerGlow",
+				injectSelf = "innerGlow",
+				texture = [[Interface\SpellActivationOverlay\IconAlert]],
+				size = 53,
+				--setAllPoints = true,
+				points = { point = "CENTER" },
+				texCoord = { 0.00781250, 0.50781250, 0.27734375, 0.52734375 }
+			},
+			{
+				layer = "OVERLAY",
+				parentName = "Ants",
+				injectSelf = "ants",
+				texture = [[Interface\SpellActivationOverlay\IconAlertAnts]],
+				size = 44,
+				points = { point = "CENTER" }
+			}
+		},
+		scripts = {
+			OnUpdate = function(self, elapsed) AnimateTexCoords(self.ants, 256, 256, 48, 48, 22, elapsed, 0.01); end
+		}
+	});
+
+	local offsetY = 0;
+	local offsetX = 0;
+
+	local hookFunc = function(self) self:Hide(); end
+	local leaveFunc = function(self) glow:Hide(); GameTooltip:Hide(); end
+	local enterFunc = function(self)
+		glow:SetPoint("CENTER", self);
+		glow:Show();
+		GameTooltip:SetOwner(self, "ANCHOR_RIGHT");
+	    GameTooltip:SetHyperlink(self.link);
+	    GameTooltip:Show();
+	end
+
+	for i = 1, 90 do
+		if i ~= 9 then -- Skip 10th to allow neatness with close button.
+			local button = _K:Frame({
+				type = "BUTTON",
+				parent = self.disenchantFrame,
+				parentName = "ItemButton" .. i,
+				inherit = "ItemButtonTemplate,SecureActionButtonTemplate",
+				textures = {
+					injectSelf = "backdrop",
+					layer = "BACKGROUND",
+					texture = [[Interface\Buttons\UI-EmptySlot-Disabled]],
+					size = 54,
+				},
+				points = {
+					point = "TOPLEFT",
+					x = 38 + (38 * offsetX),
+					y = -35 + (offsetY * -38)
+				},
+				scripts = {
+					OnEnter = enterFunc,
+					OnLeave = leaveFunc
+				},
+			});
+
+			button:HookScript("OnClick", hookFunc);
+			button:SetAttribute("type", "macro");
+			_M.itemButtons[#_M.itemButtons + 1] = button;
+		end
+
+		if i % 9 == 0 then
+			offsetY = offsetY + 1;
+			offsetX = 0;
+		else
+			offsetX = offsetX + 1;
+		end
+	end
 end
 
 _M.OpenWindow = function(self)
@@ -197,6 +320,7 @@ _M.OpenWindow = function(self)
 		self:CreateDisenchantFrame();
 	end
 
+	self.UpdateItems();
 	self.disenchantFrame:Show();
 	PlaySound("UI_EtherealWindow_Open");
 end
